8ff2317374c419f5c5a376870d8723320d2d9de0
Eric <eric@Zipcoders-MacBook-Pro-6.local>
2022-07-11 13:12:51 -0400

    fixed diagonal issue

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -1,5 +1,7 @@
 package rocks.zipcodewilmington.tictactoe;
 
+import java.util.Objects;
+
 /**
  * @author leon on 6/22/18.
  */
@@ -10,25 +12,27 @@ public class Board {
     }
 
     public Boolean isInFavorOfX() {
-        if (((this.board[0][0] == 'X') && (this.board[0][1] == 'X') && (this.board[0][2] == 'X')) ||
-                ((this.board[1][0] == 'X') && (this.board[1][1] == 'X') && (this.board[1][2] == 'X')) ||
-                ((this.board[2][0] == 'X') && (this.board[2][1] == 'X') && (this.board[2][2] == 'X')) ||
-                ((this.board[0][0] == 'X') && (this.board[1][1] == 'X') && (this.board[2][2] == 'X')) ||
-                ((this.board[0][0] == 'X') && (this.board[1][0] == 'X') && (this.board[2][0] == 'X')) ||
-                ((this.board[0][1] == 'X') && (this.board[1][1] == 'X') && (this.board[2][1] == 'X')) ||
-                ((this.board[0][2] == 'X') && (this.board[1][2] == 'X') && (this.board[2][2] == 'X'))) {
+        if (((board[0][0] == 'X') && (board[0][1] == 'X') && (board[0][2] == 'X')) ||
+                ((board[1][0] == 'X') && (board[1][1] == 'X') && (board[1][2] == 'X')) ||
+                ((board[2][0] == 'X') && (board[2][1] == 'X') && (board[2][2] == 'X')) ||
+                ((board[0][0] == 'X') && (board[1][1] == 'X') && (board[2][2] == 'X')) ||
+                ((board[0][2] == 'X') && (board[1][1] == 'X') && (board[2][0] == 'X')) ||
+                ((board[0][0] == 'X') && (board[1][0] == 'X') && (board[2][0] == 'X')) ||
+                ((board[0][1] == 'X') && (board[1][1] == 'X') && (board[2][1] == 'X')) ||
+                ((board[0][2] == 'X') && (board[1][2] == 'X') && (board[2][2] == 'X'))) {
             return true;
         } else {return false;}
     }
 
     public Boolean isInFavorOfO() {
-        if (((this.board[0][0] == 'O') && (this.board[0][1] == 'O') && (this.board[0][2] == 'O')) ||
-                ((this.board[1][0] == 'O') && (this.board[1][1] == 'O') && (this.board[1][2] == 'O')) ||
-                ((this.board[2][0] == 'O') && (this.board[2][1] == 'O') && (this.board[2][2] == 'O')) ||
-                ((this.board[0][0] == 'O') && (this.board[1][1] == 'O') && (this.board[2][2] == 'O')) ||
-                ((this.board[0][0] == 'O') && (this.board[1][0] == 'O') && (this.board[2][0] == 'O')) ||
-                ((this.board[0][1] == 'O') && (this.board[1][1] == 'O') && (this.board[2][1] == 'O')) ||
-                ((this.board[0][2] == 'O') && (this.board[1][2] == 'O') && (this.board[2][2] == 'O'))
+        if (((board[0][0] == 'O') && (board[0][1] == 'O') && (board[0][2] == 'O')) ||
+                ((board[1][0] == 'O') && (board[1][1] == 'O') && (board[1][2] == 'O')) ||
+                ((board[2][0] == 'O') && (board[2][1] == 'O') && (board[2][2] == 'O')) ||
+                ((board[0][0] == 'O') && (board[1][1] == 'O') && (board[2][2] == 'O')) ||
+                ((board[0][2] == 'O') && (board[1][1] == 'O') && (board[2][0] == 'O')) ||
+                ((board[0][0] == 'O') && (board[1][0] == 'O') && (board[2][0] == 'O')) ||
+                ((board[0][1] == 'O') && (board[1][1] == 'O') && (board[2][1] == 'O')) ||
+                ((board[0][2] == 'O') && (board[1][2] == 'O') && (board[2][2] == 'O'))
         ) {
             return true;
         } else {return false;}
@@ -36,17 +40,13 @@ public class Board {
 
     public Boolean isTie() {
         String tie = getWinner();
-        if (tie == ""){
-            return true;
-        } else {
-            return false;
-        }
+        return Objects.equals(tie, "");
     }
 
     public String getWinner() {
-        if(isInFavorOfX() == true){
+        if(isInFavorOfX()){
             return "X";
-        } else if (isInFavorOfO() == true){
+        } else if (isInFavorOfO()){
             return "O";
         }else return "";
     }
