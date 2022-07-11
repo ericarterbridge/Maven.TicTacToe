ddcd7253578acd939e4185f69c0f4724bb0cb54a
Eric <eric@Zipcoders-MacBook-Pro-6.local>
2022-07-11 12:29:38 -0400

    passes tests

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,51 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] board;
     public Board(Character[][] matrix) {
+       this.board = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        if (((this.board[0][0] == 'X') && (this.board[0][1] == 'X') && (this.board[0][2] == 'X')) ||
+                ((this.board[1][0] == 'X') && (this.board[1][1] == 'X') && (this.board[1][2] == 'X')) ||
+                ((this.board[2][0] == 'X') && (this.board[2][1] == 'X') && (this.board[2][2] == 'X')) ||
+                ((this.board[0][0] == 'X') && (this.board[1][1] == 'X') && (this.board[2][2] == 'X')) ||
+                ((this.board[0][0] == 'X') && (this.board[1][0] == 'X') && (this.board[2][0] == 'X')) ||
+                ((this.board[0][1] == 'X') && (this.board[1][1] == 'X') && (this.board[2][1] == 'X')) ||
+                ((this.board[0][2] == 'X') && (this.board[1][2] == 'X') && (this.board[2][2] == 'X'))) {
+            return true;
+        } else {return false;}
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        if (((this.board[0][0] == 'O') && (this.board[0][1] == 'O') && (this.board[0][2] == 'O')) ||
+                ((this.board[1][0] == 'O') && (this.board[1][1] == 'O') && (this.board[1][2] == 'O')) ||
+                ((this.board[2][0] == 'O') && (this.board[2][1] == 'O') && (this.board[2][2] == 'O')) ||
+                ((this.board[0][0] == 'O') && (this.board[1][1] == 'O') && (this.board[2][2] == 'O')) ||
+                ((this.board[0][0] == 'O') && (this.board[1][0] == 'O') && (this.board[2][0] == 'O')) ||
+                ((this.board[0][1] == 'O') && (this.board[1][1] == 'O') && (this.board[2][1] == 'O')) ||
+                ((this.board[0][2] == 'O') && (this.board[1][2] == 'O') && (this.board[2][2] == 'O'))
+        ) {
+            return true;
+        } else {return false;}
     }
 
     public Boolean isTie() {
-        return null;
+        String tie = getWinner();
+        if (tie == ""){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public String getWinner() {
-        return null;
+        if(isInFavorOfX() == true){
+            return "X";
+        } else if (isInFavorOfO() == true){
+            return "O";
+        }else return "";
     }
 
 }
